e800f3016340eecafad002565e4e6410ed2d649c
rayniervanegmond <raynierx@gmail.com>
2022-09-24 15:22:04 -0700

    Update the front page with personal data.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,11 @@
   <div class="hello">
     <h1>Raynier van Egmond - Personal Profile</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      This page provides an overview of my work in the following areas:
+
+      * SatelliteRemote Sensing and Deep Learning
+      * Automated Reasoning and Formal Modal Logic
+      * Digital Financial Reporting and Semantics
     </p>
   </div>
 </template>
