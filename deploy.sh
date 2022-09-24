5b0c4dd44045edcc4008b706129cbf7e4dce6023
rayniervanegmond <raynierx@gmail.com>
2022-09-24 15:18:48 -0700

    Update home page

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd dist
 
 REPONAME=personal-profile
 
-# git init
+git init
 git add -A 
 git commit -m "emptied home page"
 git push -f https://github.com/rayniervanegmond/personal-profile main:gh-pages
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
     <h1>Raynier van Egmond - Personal Profile</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
   </div>
 </template>
 
